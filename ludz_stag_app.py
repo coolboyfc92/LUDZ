9db7bf4cea9feba4600c6e1d837ae05819981362
Matt Husselbury <matthew.husselbury@gmail.com>
2025-08-30 20:05:40 +0100

    Update ludz_stag_app.py

--- ludz_stag_app.py
+++ ludz_stag_app.py
@@ -216,6 +216,19 @@ with tab1:
     else:
         st.write("🍺 The Easter egg has already been discovered and used!")
 
+# Centered video embed
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center; margin-top: 20px;">
+        <iframe width="560" height="315"
+        src="https://www.youtube.com/embed/gxwWUiZ9b9M?si=yc-zy1xCvXpOLtPH"
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
 # -------------------- PUB RULES TAB --------------------
 with tab2:
     st.header("📖 Pub Rules")
